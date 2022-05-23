211a02fe7881657d52a5ea302e13b5ff861e3c8f
FalseMirror1 <xanderignatiev@gmail.com>
2022-05-23 15:13:10 +0300

    Init

---
+++ advertisements/serializers.py
@@ -0,0 +1,46 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
+
+from advertisements.models import Advertisement
+
+
+class UserSerializer(serializers.ModelSerializer):
+    """Serializer для пользователя."""
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'first_name',
+                  'last_name',)
+
+
+class AdvertisementSerializer(serializers.ModelSerializer):
+    """Serializer для объявления."""
+
+    creator = UserSerializer(read_only=True, )
+
+    class Meta:
+        model = Advertisement
+        fields = ('id', 'title', 'description', 'creator',
+                  'status', 'created_at', )
+
+    def create(self, validated_data):
+        """Метод для создания"""
+
+        validated_data["creator"] = self.context["request"].user
+        return super().create(validated_data)
+
+    def validate(self, data):
+        """Метод для валидации. Вызывается при создании и обновлении."""
+
+        req = self.context['request']
+
+        if req.method == 'POST':
+            if len(Advertisement.objects.filter(creator=req.user, status='OPEN')) > 9:
+                raise ValidationError('Максимум открытых объявлений - 10!')
+
+        if req.method == 'PATCH' and data.get('status') == 'OPEN':
+            if len(Advertisement.objects.filter(creator=req.user, status='OPEN')) > 9:
+                raise ValidationError('Максимум открытых объявлений - 10!')
+
+        return data
